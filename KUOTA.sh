181305726233092177f9e6241beb682719e2f547
TAMVANgithub <52382931+TAMVANgithub@users.noreply.github.com>
2019-07-05 11:02:04 +0700

    Add files via upload

---
+++ KUOTA.sh
@@ -0,0 +1,199 @@
+#!/bin/bash 
+
+#Maafkan Kegabutan Ku :\/
+
+#bwahaha
+
+
+
+ blue='\033[34;1m'
+
+green='\033[32;1m'                                              
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m'                                                 
+
+yellow='\033[33;1m'
+
+
+
+echo $red "SEBUT NAMAKU 3 KALI"
+
+sleep 1
+
+clear
+
+echo $blue "NAMADISAMARKAN"
+
+sleep 1
+
+clear
+
+echo $green "ORANGTAMVAN"
+
+sleep 1
+
+clear
+
+echo $red "NAMADISMARKAN"
+
+sleep 
+
+clear
+
+echo $blue "ORANGTAMVAN"
+
+sleep 1
+
+clear
+
+echo $green "NAMADISAMARKAN"
+
+sleep 1
+
+clear
+
+echo $red "ORANGTAMVAN"
+
+sleep 1
+
+clear
+
+echo $blue ""
+
+sleep 1
+
+clear
+
+echo $green "😎😎😎😎😎😎😎😎"
+
+sleep 1
+
+clear
+
+
+
+ echo "
+
++=====$$$$$==================+
+
++ dibuat oleh : NAMADISAMARKAN      +
+
++ Disponsori oleh : Orangtamavn  +
+
++ Tujuan Tool:Hanya Untuk Bersenang Senang    +
+
++==============================+
+
+ 
+
+ ##### KoutaFree(Tree) #####  ";
+
+echo $green"menu kuota Gratis iy gratis";
+
+echo $blue"1.100GB (Tree)";
+
+echo $purple"2.1JUTAGB (Tree)";
+
+echo $yellow"3.50GB (Tree)";
+
+echo $green"4.500GB (Tree)"
+
+echo $blue"5.10TB(Tree)"
+
+echo $red"00.Ini Out"
+
+read -p "[>>" stc
+
+
+
+if [ $stc = 1 ]
+
+then 
+
+clear
+
+echo "MASUKAN NOMORLU JANGAN ANULU"|lolcat
+
+read no
+
+echo "Wait 1ABAD"|lolcat
+
+echo "$no, Sukses "
+
+fi 
+
+
+
+if [ $stc = 2 ]
+
+then 
+
+clear
+
+echo "MASUKAN NOMORMU JANGAN ANUMU"|lolcat
+
+read no
+
+echo "Wait 1ABAD"|lolcat
+
+echo "$no, Sukses "|lolcat
+
+fi
+
+
+
+if [ $stc = 3 ]
+
+then 
+
+clear
+
+echo "MASUKAN NOMORMU JANGAN ANUMU"|lolcat
+
+read no
+
+echo "Wait 1ABAD"|lolcat
+
+echo "$no, Sukses "|lolcat
+
+fi
+
+
+
+if [ $stc = 4 ]
+
+then 
+
+clear
+
+echo "MASUKAN NOMORMU JANGAN ANUMU"|lolcat
+
+read no
+
+echo "Wait 1ABAD"|lolcat
+
+echo "$no, Sukses Gan"|lolcat
+
+fi
+
+
+
+if [ $stc = 00 ]
+
+then 
+
+clear
+
+echo "Apakah Anda percaya ini beneran 😱?"|lolcat
+
+read ya
+
+echo "$selamat anda kena prank bagikan kesmua tmnmu y🤣🤣"|lolcat
+
+fi
